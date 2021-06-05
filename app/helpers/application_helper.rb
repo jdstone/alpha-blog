260e6f8a3973ea0fbc1454317f1b9f01fe093354
J.D. Stone <jdstone@jdstone1.com>
2021-06-05 13:43:45 -0700

    Add Users show page with profile image and articles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 100})
+    email_address = user.email.downcase
+    size = options[:size]
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_src, alt: user.firstname + ' ' + user.lastname, class: "rounded shadow mx-auto d-block")
+  end
 end
