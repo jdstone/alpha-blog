5b26a34586f45a8cf3429086a2ec6c2d8dcf6f14
J.D. Stone <jdstone@jdstone1.com>
2021-06-13 13:56:32 -0700

    Restrict actions at controller level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
     # end
 
     if @user.update(user_params)
-      flash[:notice] = "Account successfully updated"
+      flash[:notice] = "Account successfully updated."
       redirect_to @user
     else
       render 'edit'
