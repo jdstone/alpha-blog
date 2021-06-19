021d5ee856408f398b0eeeecad08f3edbf7a7305
J.D. Stone <jdstone@jdstone1.com>
2021-06-19 13:57:32 -0700

    Restrict actions and add delete account functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -41,6 +43,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Your account has been successfully deleted."
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
@@ -48,5 +57,12 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:alert] = "You can only edit your account."
+      redirect_to @user
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  has_many :articles
+  has_many :articles, dependent: :destroy
   validates :firstname, presence: true, length: { minimum: 2, maximum: 45 }
   validates :lastname, presence: true, length: { minimum: 2, maximum: 45 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
