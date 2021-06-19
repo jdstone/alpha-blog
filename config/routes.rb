cb5f3a4a24b45a6b92d12af959252fab379cc211
J.D. Stone <jdstone@jdstone1.com>
2021-06-19 13:55:11 -0700

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,4 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  # get 'logout', to: 'sessions#logout'
-  # get 'logout', to: 'sessions#destroy'
 end
