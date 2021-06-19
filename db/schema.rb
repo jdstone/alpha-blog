eba7f0b6d0ca2cee2390231b2e3f51237e64a98b
J.D. Stone <jdstone@jdstone1.com>
2021-06-19 14:14:44 -0700

    Admin db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_000219) do
+ActiveRecord::Schema.define(version: 2021_06_19_205847) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_000219) do
     t.string "firstname"
     t.string "lastname"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
