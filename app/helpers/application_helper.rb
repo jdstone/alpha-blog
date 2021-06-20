e9d6bc9df51ebbdaf67131cd5c534e561a0fa9a4
J.D. Stone <jdstone@jdstone1.com>
2021-06-20 16:15:00 -0700

    Alert color based on type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
     image_tag(image_src, alt: "#{user.firstname} #{user.lastname}", class: css)
   end
+
+  def bootstrap_flash_class_for flash_type
+    case flash_type
+      when "alert"
+        "alert-danger"
+      when "notice"
+        "alert-success"
+      else
+        "alert-info"
+    end
+  end
 end
