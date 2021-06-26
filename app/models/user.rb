1c720908c049b9395bdbb937edb98c9ac9c3e878
J.D. Stone <jdstone@jdstone1.com>
2021-06-26 16:05:04 -0700

    Cleanup user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,24 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 105 }
   has_secure_password
-  # attr_accessor :current_password
-  # validate :check_if_current_password_is_correct, on: :update
 
-  # def check_if_current_password_is_correct
-  #   unless password.blank?
-  #     user = User.find_by_id(id)
-  #     if (user.authenticate(current_password) == false)
-  #       # flash[:alert] = "Your current password is incorrect"
-  #       errors.add(:current_password, "is incorrect.")
-  #     end
-  #   end
-  # end
+  attr_accessor :current_password, :new_password
+
+  validate :current_password_is_correct?, if: :validate_password?, on: :update
+
+  def current_password_is_correct?
+    # For some stupid reason authenticate always returns false when called on self
+    if User.find(id).authenticate(current_password) == false
+      errors.add(:current_password, "is incorrect.")
+    end
+  end
+
+  def validate_password?
+    # if (!new_password.blank? && !password_confirmation.blank?)
+    if !password.blank?
+      true
+    else
+      false
+    end
+  end
 end
