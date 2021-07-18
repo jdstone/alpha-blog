be73eef46b5064cc2250e450fa41725559b7c4c5
J.D. Stone <jdstone@jdstone1.com>
2021-07-17 19:58:27 -0700

    Reorder alphabetically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def gravatar_for(user, options = {})
-    email_address = user.email.downcase
-    size = options[:size] || 100
-    css = options[:css] || "rounded shadow mx-auto d-block"
-    hash = Digest::MD5.hexdigest(email_address)
-    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-    image_tag(image_src, alt: "#{user.firstname} #{user.lastname}", class: css)
-  end
-
   def bootstrap_flash_class_for flash_type
     case flash_type
       when "alert"
@@ -17,5 +8,14 @@ module ApplicationHelper
       else
         "alert-info"
     end
+  end
+
+  def gravatar_for(user, options = {})
+    email_address = user.email.downcase
+    size = options[:size] || 100
+    css = options[:css] || "rounded shadow mx-auto d-block"
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_src, alt: "#{user.firstname} #{user.lastname}", class: css)
   end
 end
