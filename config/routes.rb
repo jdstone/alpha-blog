443061ab09eb9f1a510821db6d29b6d98523649c
J.D. Stone <jdstone@jdstone1.com>
2021-07-17 20:01:21 -0700

    Remove About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
