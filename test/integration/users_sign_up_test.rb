36f4b5ddd423fc0caacb5c8f2046cdce1e798304
J.D. Stone <jdstone@jdstone1.com>
2021-07-18 17:21:55 -0700

    Create user sign-up integration test

---
+++ test/integration/users_sign_up_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class UsersSignUpTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "get signup form and create user" do
+    get "/signup"
+    assert_response :success
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { firstname: "John", lastname: "Doe", email: "johndoe@example.com", password: "password" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Welcome John, thanks for signing up!", response.body
+  end
+end
